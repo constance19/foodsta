38cbc46aa0f54bd1cbabf9df5d205f7e0eed1140
constance19 <67133685+constance19@users.noreply.github.com>
2021-08-04 22:39:04 -0700

    custom selected cell UI

--- foodsta/Views/LocationCell.m
+++ foodsta/Views/LocationCell.m
@@ -18,6 +18,10 @@
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
+    UIColor *myGreen = [UIColor colorWithRed:0.429 green:0.604 blue:0.576 alpha:0.3];
+    UIView *bgColorView = [[UIView alloc] init];
+    bgColorView.backgroundColor = myGreen;
+    [self setSelectedBackgroundView:bgColorView];
 }
 
 @end
